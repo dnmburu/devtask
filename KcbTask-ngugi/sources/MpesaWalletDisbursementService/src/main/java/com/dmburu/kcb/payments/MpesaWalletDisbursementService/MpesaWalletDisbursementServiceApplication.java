7dbe4e7b026e10c0f81dbdc82a9bb5827f51f838
David Mburu <davidm3020@gmail.com>
2020-11-12 11:38:12 +0300

    Added code from spring initializer

---
+++ KcbTask-ngugi/sources/MpesaWalletDisbursementService/src/main/java/com/dmburu/kcb/payments/MpesaWalletDisbursementService/MpesaWalletDisbursementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dmburu.kcb.payments.MpesaWalletDisbursementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaWalletDisbursementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpesaWalletDisbursementServiceApplication.class, args);
+	}
+
+}
---
+++ KcbTask-ngugi/sources/MpesaWalletDisbursementService/src/test/java/com/dmburu/kcb/payments/MpesaWalletDisbursementService/MpesaWalletDisbursementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmburu.kcb.payments.MpesaWalletDisbursementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaWalletDisbursementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
